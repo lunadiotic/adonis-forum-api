0ae762025a19ed8183167e455a0f59d49999583e
aimeliala <idstack@outlook.co.id>
2023-04-17 18:55:15 +0700

    make model and seeder for category

---
+++ database/seeders/CategorySeeder.ts
@@ -0,0 +1,22 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Category from 'App/Models/Category'
+
+export default class CategorySeeder extends BaseSeeder {
+  public async run() {
+    // Write your database queries inside the run method
+    await Category.createMany([
+      {
+        title: 'Food',
+      },
+      {
+        title: 'Travel',
+      },
+      {
+        title: 'Entertainment',
+      },
+      {
+        title: 'Health',
+      },
+    ])
+  }
+}
