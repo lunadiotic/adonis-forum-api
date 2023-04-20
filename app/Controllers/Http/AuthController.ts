2b5af901525f895aa64444f4b553b2e641cf0001
aimeliala <idstack@outlook.co.id>
2023-04-20 14:36:51 +0700

    make login api endpoint with modify response

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -3,13 +3,31 @@ import User from 'App/Models/User'
 import RegisterValidator from 'App/Validators/RegisterValidator'
 
 export default class AuthController {
-  public async register({ request, auth }: HttpContextContract) {
+  public async register({ request, auth, response }: HttpContextContract) {
     const validatedData = await request.validate(RegisterValidator)
 
     const user = await User.create(validatedData)
 
     const token = await auth.login(user)
 
-    return token
+    return response.status(201).json({
+      user,
+      token,
+    })
+  }
+
+  public async login({ request, auth, response }: HttpContextContract) {
+    const { email, password } = await request.all()
+    try {
+      const token = await auth.attempt(email, password)
+      return response.status(200).json({
+        user: token.user,
+        token: token.token,
+      })
+    } catch (error) {
+      return response.status(401).json({
+        message: error.message,
+      })
+    }
   }
 }
