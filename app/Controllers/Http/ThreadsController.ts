4f4f111ef3fb01315d754196233ae6573da21524
aimeliala <idstack@outlook.co.id>
2023-04-21 13:24:13 +0700

    make delete thread api endpoint

--- app/Controllers/Http/ThreadsController.ts
+++ app/Controllers/Http/ThreadsController.ts
@@ -67,4 +67,18 @@ export default class ThreadsController {
       })
     }
   }
+
+  public async destroy({ params, response }: HttpContextContract) {
+    try {
+      const thread = await Thread.findOrFail(params.id)
+      await thread.delete()
+      return response.status(200).json({
+        message: 'Thread deleted successfully',
+      })
+    } catch (error) {
+      return response.status(400).json({
+        message: error,
+      })
+    }
+  }
 }
